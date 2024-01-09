b47354176f210c44478dc999de5634bee2bd9734
Aral Hudaoglu <aralhudaoglu23@gmail.com>
2024-01-09 16:51:35 +0200

    Navbar and menus

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
-import "./App.css";
-import getUsers from "../src/services/getUsers";
-import useAPI from "../src/effects/useAPI";
+import "./index.css";
+import Navbar from "./components/Navbar";
+import { Routes } from "react-router-dom";
 
-function App() {
-  const [usersLoading, usersError, usersResponse] = useAPI(() => getUsers());
 
-  if (usersLoading) {
-    <h1>Loading...</h1>;
-  }
-  if (usersError) {
-    <h1>Error...</h1>;
-  }
-
-  const { users } = usersResponse;
-  return (
-    <div className="App">
-      <table>
-        <tbody>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-          </tr>
-          {users?.map((user, index) => {
-            return (
-              <tr key={index}>
-                <td>{user.firstName}</td>
-                <td>{user.lastName}</td>
-                <td>{user.email}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="App">
+           <Navbar/>
+        </div>
+    )
 }
-
-export default App;
---
+++ src/components/MenuItems.js
@@ -0,0 +1,42 @@
+export const MenuItems = [
+    {
+        title : "Home",
+        url :"/",
+        cName :"nav-links",
+        icon : "fa-solid fa-house-chimney"
+    },
+    {
+        title : "About",
+        url :"/about",
+        cName :"nav-links",
+        icon : "fa-solid fa-circle-info"
+    },
+
+    {
+        title : "Bookings",
+        url :"/bookings",
+        cName :"nav-links",
+        icon : "fa-solid fa-plane-departure"
+    },
+
+    
+
+    {
+        title : "Inclusive",
+        url :"/inclusive",
+        cName :"nav-links",
+        icon : "fa-solid fa-briefcase"
+    },
+    {
+        title : "Contact",
+        url :"/contact",
+        cName :"nav-links",
+        icon : "fa-solid fa-address-book"
+    },
+    {
+        title : "Sign Up",
+        url :"/signup",
+        cName :"nav-links-mobile",
+    }
+
+]
