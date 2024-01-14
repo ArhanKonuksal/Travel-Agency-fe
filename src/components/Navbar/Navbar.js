5eb00d35d808042f98cdd3e3a193a7d0eb5bf2ed
Aral Hudaoglu <aralhudaoglu23@gmail.com>
2024-01-14 13:28:06 +0200

    Front endin son hali

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import "../Navbar/NavbarStyles.css";
 import { MenuItems } from "./MenuItems";
 import { Link } from "react-router-dom";
 
+
 class Navbar extends Component{
     state = { clicked: false };
     handleClick = () =>{
